8a71cd79991ea67eff357a3ece9c51e4fead9338
ponpppher <genmaier@gmail.com>
2019-07-24 22:12:22 +0900

    add: v-if

--- neko/codetemplate/main.js
+++ neko/codetemplate/main.js
@@ -1,10 +1,12 @@
 var app = new Vue({
   el: '#app',
   data: {
-    list: ['apple', 'banana', 'rat']
+    list: ['apple', 'banana', 'rat'], //item list
+    message: "first message", // input model
+    show: true //show paragraph
   },
   methods: {
-    handleClick: function(e) {
+    handleClick: function(e) { //click eent
       alert(e.target)
     }
   }
