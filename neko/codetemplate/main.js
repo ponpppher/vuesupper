e929986ef38d0d0a2807f27d85555b0ddbe2465e
ponpppher <genmaier@gmail.com>
2019-07-25 21:28:01 +0900

    increment count

--- neko/codetemplate/main.js
+++ neko/codetemplate/main.js
@@ -4,11 +4,15 @@ var app = new Vue({
     list: ['apple', 'banana', 'rat'], //item list
     message: "first message", // input model
     num_message: "number input", // input number model
-    show: true //show paragraph
+    show: true, //show paragraph
+    click_count: 0
   },
   methods: {
     handleClick: function(e) { //click event
       alert(e.target)
+    },
+    increment: function() { //increment count
+      this.click_count += 1
     }
   }
 })
