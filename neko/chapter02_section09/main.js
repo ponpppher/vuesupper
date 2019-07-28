ef1fab50130e83c9ce5f81991c075b595d73da27
ponpppher <genmaier@gmail.com>
2019-07-29 03:03:32 +0900

    input monster name

--- neko/chapter02_section09/main.js
+++ neko/chapter02_section09/main.js
@@ -15,5 +15,16 @@ var app = new Vue({
     ]
   },
   methods: {
-  }
+    doAdd: function() {
+      var max = this.list.reduce(function(a, b){
+        return a.id > b.id ? a.id : b.id
+      }, 0)
+      this.list.push({
+        id: max + 1,
+        name: this.name,
+        hp: 500
+      })
+      this.name = ''
+    }
+  },
 })
