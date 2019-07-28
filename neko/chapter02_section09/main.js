c7b7e327c8bd2c0d74cb07e0de992d889577b477
ponpppher <genmaier@gmail.com>
2019-07-29 02:09:43 +0900

    v-if and list data

--- neko/chapter02_section09/main.js
+++ neko/chapter02_section09/main.js
@@ -1,7 +1,13 @@
 var app = new Vue({
   el: '#app',
   data: {
-
+    ok: true,
+    type: 'B',
+    list: [
+      { id: 1, name: 'slim', hp: 100 },
+      { id: 2, name: 'goblin', hp: 200 },
+      { id: 3, name: 'dragon', hp: 500 },
+    ]
   },
   methods: {
   }
