be15a7d721e4aa958fd67383580d3978d9ceaa4b
ponpppher <genmaier@gmail.com>
2019-07-29 02:20:09 +0900

    add: index key in list

--- neko/chapter02_section09/main.js
+++ neko/chapter02_section09/main.js
@@ -7,6 +7,11 @@ var app = new Vue({
       { id: 1, name: 'slim', hp: 100 },
       { id: 2, name: 'goblin', hp: 200 },
       { id: 3, name: 'dragon', hp: 500 },
+    ],
+    keylist: [
+      { key: 'nnnn', id: 1, name: 'slim', hp: 100 },
+      { key: 'kokoro', id: 2, name: 'goblin', hp: 200 },
+      { key: 'aa', id: 3, name: 'dragon', hp: 500 },
     ]
   },
   methods: {
